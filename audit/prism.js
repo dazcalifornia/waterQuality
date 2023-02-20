ee9d8dd537c249c8d68213b9a2fb8b88ce5d46e6
Supakit Wiwustrinon <divertzt@gmail.com>
2023-02-20 16:19:10 +0700

    adding contents

---
+++ audit/prism.js
@@ -0,0 +1,9 @@
+
+import Prism from 'prismjs';
+
+// Import the languages you want to support
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-python';
+
+export default Prism;
