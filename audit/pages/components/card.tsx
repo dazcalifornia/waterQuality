b625165b2e5715f6007d9d33d17a7b939c10abee
dazcalifornia <divertzt@gmail.com>
2023-03-02 14:06:11 +0700

    UI Changes

--- audit/pages/components/card.tsx
+++ audit/pages/components/card.tsx
@@ -1,9 +1,18 @@
 export default function Card(){
   return (
-    <>
-    <div className="card">
-      <p>HELLO FROM ANITHER WORLD </p> 
-    </div>
-    </>
+      <div className="container">
+      <div className="feed">
+        <div className="tweet">
+          <div className="avatar">
+            <img  src="https://cdn.discordapp.com/attachments/884526941984665600/1080746791894515782/IMG_1490.png" alt="profile"/>
+          </div>
+          <div className="content">
+            <div className="name">Franx</div>
+            <div className="username">@Alexis</div>
+            <div className="text">Test</div>
+          </div>
+        </div>
+      </div>
+    </div> 
   )
 }
