f95d91bd7ee2426693f0db483965d033698994c0
dazcalifornia <divertzt@gmail.com>
2023-03-09 06:44:24 +0700

    -fixes vercel err

--- audit/pages/components/todoCard.tsx
+++ audit/pages/components/todoCard.tsx
@@ -3,14 +3,20 @@ import PocketBase from "pocketbase";
 
 const pb = new PocketBase("http://127.0.0.1:8090");
 
+type Todo = {
+  id: string;
+  task: string;
+  created: string;
+}
+
 export default function TodoList() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<Todo[]>([]); // Add the Todo type and an empty array
   const [error, setError] = useState("");
 
   useEffect(() => {
     pb.collection("todo")
       .getFullList(200, { sort: "-created" })
-      .then((list) => {
+      .then((list: Todo[]) => { // Annotate the type of list as Todo[]
         setTodos(list);
         console.log(list)
       })
