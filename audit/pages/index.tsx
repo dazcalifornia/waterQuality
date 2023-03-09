23f695e724fb596b88d013d9c3629c672c34054b
dazcalifornia <divertzt@gmail.com>
2023-03-09 07:17:27 +0700

    -fixes vercel err

--- audit/pages/index.tsx
+++ audit/pages/index.tsx
@@ -2,80 +2,101 @@ import Head from 'next/head';
 import { useState, useEffect, useRef } from 'react';
 import PocketBase from 'pocketbase';
 
-import Card from './components/card';
-import StatusCard from './components/status';
 import ActivityCard from './components/activity';
 import ProductionCard from './components/production';
 import TodoList from './components/todoCard';
 
-export default function Home() {
+interface Activity {
+  title: string;
+  desc: string;
+  status: string;
+  date: string;
+}
 
-  //fetch ACtivity Data 
-  const [activityData, setActivityData] = useState(null);
-  const [productionData, setProductionData] = useState(null);
+interface Production {
+  title: string;
+  desc: string;
+  status: string;
+  date: string;
+}
+
+export default function Home() {
+  //fetch Activity Data
+  const [activityData, setActivityData] = useState<Activity[]>([]);
+  const [productionData, setProductionData] = useState<Production[]>([]);
 
   useEffect(() => {
-    const pb = new PocketBase("http://127.0.0.1:8090");
-    const actRec = pb.collection("activity").getFullList(200,{
-      sort: '-created'
-    }).then((act) => {
-      const formattedActivities = act.map((activity) => {
-        const date = new Date(activity.date);
-        const formattedDate = date.toLocaleDateString();
-        return { ...activity, date: formattedDate };
+    const pb = new PocketBase('http://127.0.0.1:8090');
+    const actRec = pb
+      .collection('activity')
+      .getFullList(200, {
+        sort: '-created',
+      })
+      .then(act => {
+        const formattedActivities = act.map(activity => {
+          const { title, desc, status  } = activity;
+          const date = new Date(activity.date);
+          const formattedDate = date.toLocaleDateString();
+          return { title, desc, status, date: formattedDate }as Activity;
+        });
+        setActivityData(formattedActivities);
+      })
+      .catch(error => {
+        console.error(error);
+        // handle error
+        setActivityData(error);
       });
-      setActivityData(formattedActivities);
-    }).catch((error) => {
-      console.error(error);
-      // handle error
-      setActivityData(error);
-    });
-    const prodRec = pb.collection("productions").getFullList(200,{
-      sort: '-created'
-    }).then((prod) => {
-      const formattedProductions = prod.map((production) => {
-        const date = new Date(production.date);
-        const formattedDate = date.toLocaleDateString();
-        return { ...production, date: formattedDate };
+    const prodRec = pb
+      .collection('productions')
+      .getFullList(200, {
+        sort: '-created',
+      })
+      .then(prod => {
+        const formattedProductions = prod.map(production => {
+          const { title, desc, status, date } = production;
+          const formattedDate = new Date(date).toLocaleDateString();
+          return { title, desc, status, date: formattedDate } as Production;
+        });
+        setProductionData(formattedProductions);
+      })
+      .catch(error => {
+        console.error(error);
+        // handle error
+        setProductionData(error);
       });
-      setProductionData(formattedProductions);
-    }).catch((error) => {
-      console.error(error);
-      // handle error
-      setProductionData(error);
-    });
-
-
   }, []);
 
   return (
-    <div >
-      
+    <div>
       <Head>
         <title>Progression Web Page 📦</title>
         <meta name="description" content="This web will show what I'm doin " />
         <link rel="icon" href="/icon.png" />
       </Head>
       <div className="row">
-	      <div className="column left-column card">
-          {productionData?.map((production) => (
+        <div className="column left-column card">
+          {productionData.map(production => (
             <ProductionCard
-            title={production.title} 
-            description={production.desc} 
-            status={production.status}
-            date={production.date}/>
+              title={production.title}
+              description={production.desc}
+              status={production.status}
+              date={production.date}
+            />
           ))}
-
         </div>
         <div className="column card">
-          {activityData?.map((act) => (
-            <ActivityCard title={act.title} description={act.desc} date={act.date}/>
+          {activityData.map(act => (
+            <ActivityCard
+              title={act.title}
+              description={act.desc}
+              date={act.date}
+            />
           ))}
         </div>
-	      	<div className="column card">
-            <TodoList />
-	      	</div>
-	      </div>
+        <div className="column card">
+          <TodoList />
+        </div>
+      </div>
     </div>
   );
 }
