4f659000b04aaf5e401e4443b8336f8aab862d47
Supakit Wiwustrinon <divertzt@gmail.com>
2023-05-18 11:12:18 +0700

    adding matterial UI

--- webserv/pages/index.tsx
+++ webserv/pages/index.tsx
@@ -1,35 +1,18 @@
 import Link from "next/link"
+
+import Navbar from "./nav"
+import TopNavs from "./topNav"
 export default function Home() {
   return (
     <>
-      <header>
-        <h2>under construction</h2>
-      </header>
+      <TopNavs  />
         <main>
           <p>
             under construction
           </p>
         </main>
         <p>© 2021 - 2022</p>
-
-      <footer>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/about">
-                <i className="fas fa-info-circle"></i>
-                <p>About</p>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-                <i className="fas fa-envelope"></i>
-                <p>Contact</p>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </footer>
+        <Navbar />
     </>
   )
 }
