b7db0db63ec2e8a856fd1ccfc9df77920a6530ae
dazcalifornia <divertzt@gmail.com>
2023-07-11 05:45:33 +0700

    map and forecastTable

--- webserv/pages/forecast.tsx
+++ webserv/pages/forecast.tsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
+
+interface ForecastData {
+  Datetime: string;
+  Salinity: number;
+  Turbidity: number;
+  SeaTemp: number;
+  DOSaturation: number;
+  Chlorophyll: number;
+}
 
 const ForecastPage = () => {
+  const [forecastData, setForecastData] = useState<ForecastData[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<ForecastData[]>('http://localhost:8000/forecast')
+      .then((response) => {
+        setForecastData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Forecast</h1>
+      {forecastData.length > 0 ? (
+        <TableContainer sx={{ overflowX: 'auto' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Date</TableCell>
+                <TableCell>Salinity</TableCell>
+                <TableCell>Turbidity</TableCell>
+                <TableCell>SeaTemp</TableCell>
+                <TableCell>DOSaturation</TableCell>
+                <TableCell>Chlorophyll</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {forecastData.map((item) => (
+                <TableRow key={item.Datetime}>
+                  <TableCell>{item.Datetime}</TableCell>
+                  <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                    {item.Salinity.toFixed(2)}
+                  </TableCell>
+                  <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                    {item.Turbidity.toFixed(2)}
+                  </TableCell>
+                  <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                    {item.SeaTemp.toFixed(2)}
+                  </TableCell>
+                  <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                    {item.DOSaturation.toFixed(2)}
+                  </TableCell>
+                  <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                    {item.Chlorophyll.toFixed(2)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <p>No forecast data available.</p>
+      )}
     </div>
   );
-}
-export default ForecastPage;
+};
 
+export default ForecastPage;
--- webserv/pages/map.tsx
+++ webserv/pages/map.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGl2ZXJ0enQiLCJhIjoiY2xqeGYybzZuMXVzMzNtbzZ6ZXNqaXNnZSJ9.JAb0ekG5zuG1wkDfkjCuJg'; // Replace with your Mapbox access token
 
 const MapPage = () => {
+  useEffect(() => {
+    // Create the map instance
+    const map = new mapboxgl.Map({
+      container: 'map', // HTML element ID where the map will be rendered
+      style: 'mapbox://styles/mapbox/streets-v11', // Mapbox style URL
+      center: [-74.5, 40], // Initial map center coordinates [longitude, latitude]
+      zoom: 9 // Initial map zoom level
+    });
+
+    // Add map controls
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // Add pinned locations as map markers
+    const locations = [
+      { name: 'Location 1', coordinates: [-74.5, 40] },
+      { name: 'Location 2', coordinates: [-74.6, 40.2] },
+      // Add more locations as needed
+    ];
+
+    locations.forEach((location) => {
+      const marker = new mapboxgl.Marker()
+        .setLngLat(location.coordinates)
+        .setPopup(new mapboxgl.Popup().setHTML(`<h3>${location.name}</h3>`))
+        .addTo(map);
+    });
+
+    // Clean up the map instance on component unmount
+    return () => map.remove();
+  }, []);
+
   return (
     <div>
       <h1>Map</h1>
+      <div id="map" style={{ width: '100%', height: '400px' }}></div>
     </div>
   );
-}
-export default MapPage;
+};
 
+export default MapPage;
